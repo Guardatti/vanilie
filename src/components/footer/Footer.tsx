5c78ed8f8354e611511edf5a3129e3fb8587ccad
Nicolas <nicolasguardattiutn@gmail.com>
2025-02-05 21:21:44 -0300

    .

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import './footer.css'
 
 const Footer: React.FC = () => {
   return (
-    <div>
-      
-    </div>
+    <></>
   )
 }
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import Slider from './Slider'
-import './home.css'
 
 const Home: React.FC = () => {
   return (
-    <section className='section'>
-      <div className='container-slider'>
-        <Slider />
-      </div>
-    </section>
+    <></>
   )
 }
 
