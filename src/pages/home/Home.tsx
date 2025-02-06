d728c9e929bcb7b7060233cf5526222630ec3753
Nicolas <nicolasguardattiutn@gmail.com>
2025-02-06 15:46:41 -0300

    .

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,81 +2,8 @@ import React from 'react'
 import Slider from './Slider'
 import './home.css'
 import { Link } from 'react-router-dom'
+import { bottoms, completeLook, upperclothing } from '../../utils/interfaces'
 
-interface Images{
-  id: number,
-  url: string,
-  alt: string,
-}
-
-const upperclothing: Images[] = [
-  {
-    id: 1,
-    url: 'img_upperclothing/remeras.avif',
-    alt: 'Remeras',
-  },
-  {
-    id: 2,
-    url: 'img_upperclothing/musculosas.avif',
-    alt: 'Musculosas',
-  },
-  {
-    id: 3,
-    url: 'img_upperclothing/camisas.avif',
-    alt: 'Camisas',
-  },
-  {
-    id: 4,
-    url: 'img_upperclothing/tops.avif',
-    alt: 'Tops',
-  },
-]
-
-const bottoms: Images[] = [
-  {
-    id: 1,
-    url: 'img_bottoms/pantalon.avif',
-    alt: 'Pantalon',
-  },
-  {
-    id: 2,
-    url: 'img_bottoms/bermuda.avif',
-    alt: 'Bermuda',
-  },
-  {
-    id: 3,
-    url: 'img_bottoms/short.avif',
-    alt: 'Short',
-  },
-  {
-    id: 4,
-    url: 'img_bottoms/pollera.avif',
-    alt: 'Pollera',
-  },
-]
-
-const completeLook: Images[] = [
-  {
-    id: 1,
-    url: 'img_completelook/vestido.avif',
-    alt: 'Vestido',
-  },
-  {
-    id: 2,
-    url: 'img_completelook/conjunto.avif',
-    alt: 'Conjunto',
-  },
-  {
-    id: 3,
-    url: 'img_completelook/cartera.avif',
-    alt: 'Cartera',
-  },
-  {
-    id: 4,
-    url: 'img_completelook/zapatilla.avif',
-    alt: 'Zapatilla',
-  },
-]
 
 const Home: React.FC = () => {
   return (
--- src/pages/home/Slider.tsx
+++ src/pages/home/Slider.tsx
@@ -6,36 +6,8 @@ import 'swiper/css/effect-fade'
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import './slider.css'
+import { sliderImages } from '../../utils/interfaces';
 
-interface SliderImages {
-    id: number,
-    url: string,
-    alt: string,
-}
-
-
-const sliderImages: SliderImages[]= [
-    {
-        id: 1,
-        url: 'img_slider/1.webp',
-        alt: 'estampa_vanilie'
-    },
-    {
-        id: 2,
-        url: 'img_slider/2.webp',
-        alt: 'local_vanilie'
-    },
-    {
-        id: 3,
-        url: 'img_slider/3.webp',
-        alt: 'local_vanilie'
-    },
-    {
-        id: 4,
-        url: 'img_slider/4.webp',
-        alt: 'local_vanilie'
-    },
-]
 
 const Slider: React.FC = () => {
   return (
