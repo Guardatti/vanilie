96e8aefb1bef4ba607b4b890f654052a47ccdaf4
Dengnis <dengnis97@gmail.com>
2025-02-05 22:28:09 -0400

    Fix: Tamaño del footer y construccion del mismo

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 import './footer.css'
 
 const Footer: React.FC = () => {
   return (
-    <></>
+    <div className='footer'>
+        <p>Copyright © VANILIE - 2025</p>
+    </div>
   )
 }
 
