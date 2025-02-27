ac2b5c39e5ce2985b5b9a2583181a8a5a7647cbd
Nicolas <nicolasguardattiutn@gmail.com>
2025-02-27 18:55:16 -0300

    .

--- src/pages/products/tshirts/Tshirts.tsx
+++ src/pages/products/tshirts/Tshirts.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../products.css'
 import { products } from '../../../components/data/products'
 import { motion } from 'framer-motion'
@@ -7,19 +7,31 @@ import { formatPrice } from '../../../utils/formatPrice'
 
 const Tshirts: React.FC = () => {
 
+  const [filter, setFilter] = useState<string>('HOMBRE Y MUJER')
 
+  const [filterPrice, setFilterPrice] = useState<string>('POR DEFECTO')
+
+  const handleChangeFilter = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilter(event.target.value)
+    console.log(filter);
+  };
+
+  const handleChangeFilterPrice = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterPrice(event.target.value)
+    console.log(filterPrice);
+  }
 
   return (
     <div className='container-product-tshirts'>
         <div className='container-tshirts-title-filter'>
             <h1 className='title-tshirts'>REMERAS</h1>
             <div className='filter-tshirts'>
-              <select className='select-tshirts'>
-                <option value="HOMBRE Y MUJER">HOMBRE Y MUJER</option>
-                <option value="HOMBRE">HOMBRE</option>
-                <option value="MUJER">MUJER</option>
+              <select className='select-tshirts' value={filter} onChange={handleChangeFilter}>
+                <option value="male">HOMBRE Y MUJER</option>
+                <option value="male">HOMBRE</option>
+                <option value="famele">MUJER</option>
               </select>
-              <select className='select-tshirts'>
+              <select className='select-tshirts' value={filterPrice} onChange={handleChangeFilterPrice}>
                 <option value="POR DEFECTO">FILTRAR</option>
                 <option value="MAYOR A MENOR">PRECIO: MAYOR A MENOR</option>
                 <option value="MENOR A MAYOR">PRECIO: MENOR A MAYOR</option>
@@ -28,7 +40,7 @@ const Tshirts: React.FC = () => {
         </div>
         <div className='container-products'>
           {
-            products.filter((product) => product.category === 'tshirts').map((product) => (
+            products.filter((product) => product.category === ' tshirts').map((product) => (
               <div className='container-product-img-info' key={product.id}>
                 <div className='container-product-img'>
                   <img className='img-product' src={product.img}/>
